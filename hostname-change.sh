23db1e953623544a6e0f0ad69f87400e4ed17e1e
eq <estuardo.quan@gmail.com>
2026-06-04 11:23:30 -0600

    some other changes

--- hostname-change.sh
+++ hostname-change.sh
@@ -6,28 +6,71 @@ log_change()
     printf "%s: %s\n" "$(date)" "${1}" >> ${LOG}
 }
 
+
+LOGONLY=0
+NSUPDATE_KEY=/var/tmp/nsupdate.key
 STATE=/var/lib/hostname/state
+
+OPT=$(getopt -o k:s: --long key:,log-only,state: -n "$0" -- "$@")
+
+if [ $? -ne 0 ]; then
+    exit 1
+fi
+
+eval set -- "${OPT}"
+
+while true; do
+    case "$1" in
+        -k|--key)
+            NSUPDATE_KEY=$2
+            shift 2
+            ;;
+        -s|--state)
+            STATE=$2
+            shift 2
+            ;;   
+        --log-only)
+            LOGONLY=1
+            shift
+            ;;   
+        --)
+            shift 
+            break
+            ;;
+        *)
+            echo "Unexpected option: $1"
+            exit 1
+            ;;
+    esac
+done
+
+
 
 mkdir -p "$(dirname ${STATE})"
 
 CURRENT=$(hostname)
 PREVIOUS=$(cat ${STATE} 2>/dev/null)
 
-NSUPDATE_KEY=/home/admin/.ddns/acme-update.key
 
 if [ -z "${PREVIOUS}" ]; then
     log_change "initialized hostname is now ${CURRENT}"
     
-    hostname-nsupdate | nsupdate -k ${NSUPDATE_KEY}
+    if [ ${LOGONLY} = 0]; then
+        hostname-nsupdate | nsupdate -k ${NSUPDATE_KEY}
+    fi
 else
     if [ "${CURRENT}" != "${PREVIOUS}" ]; then
         log_change "started hostname change from ${PREVIOUS} to ${CURRENT}"
+        
+        if [ ${LOGONLY} = 0]; then
+            hostname-nsupdate | nsupdate -k ${NSUPDATE_KEY}
 
-        hostname-nsupdate -d --host ${PREVIOUS} | nsupdate -k ${NSUPDATE_KEY}
+            hostname-nsupdate -d --host ${PREVIOUS} | nsupdate -k ${NSUPDATE_KEY}
 
-        sleep 30
+            sleep 30
 
-        hostname-nsupdate | nsupdate -k ${NSUPDATE_KEY}
+            hostname-nsupdate | nsupdate -k ${NSUPDATE_KEY}
+        fi
 
         printf "%s\n" "${CURRENT}" > ${STATE}
 
